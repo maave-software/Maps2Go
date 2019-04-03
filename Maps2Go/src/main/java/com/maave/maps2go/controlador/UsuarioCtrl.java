c8de0d3e4b877e751e105ab4a581e47a6099c3c1
AngelaJanin <angelajanin@ciencias.unam.mx>
2019-04-03 13:25:52 -0600

    Actualizar

--- Maps2Go/src/main/java/com/maave/maps2go/controlador/UsuarioCtrl.java
+++ Maps2Go/src/main/java/com/maave/maps2go/controlador/UsuarioCtrl.java
@@ -2,16 +2,31 @@ package com.maave.maps2go.controlador;
 
 import com.maave.maps2go.modelo.Usuario;
 import com.maave.maps2go.modelo.UsuarioDAO;
+import com.maave.maps2go.vista.CamposSinLlenarIH;
+import com.maave.maps2go.vista.CorreoExistenteIH;
+import com.maave.maps2go.vista.NombreExistenteIH;
+import com.maave.maps2go.vista.CuentaActualizadaIH;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean
 public class UsuarioCtrl {
     
+    private int idUsuario;
     private int rol;
     private String correo;
     private String contrasenia;
     private String nombreUsuario;
 
+    public int getIdUsuario() {
+        // Automatically generated method. Please do not modify this code.
+        return this.idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        // Automatically generated method. Please do not modify this code.
+        this.idUsuario = idUsuario;
+    }
     
     public String getNombreUsuario() {
         // Automatically generated method. Please do not modify this code.
@@ -60,13 +75,42 @@ public class UsuarioCtrl {
     }
     
     public void agregarCuenta(){
-
     }
     
     public void actualizarCuenta() {
+               UsuarioDAO udb = new UsuarioDAO();
+        Usuario usuario = udb.consultarId(idUsuario);
+        if (usuario != null){
+            if(nombreUsuario != null && !nombreUsuario.isEmpty()){
+                if(!udb.existeNombre(nombreUsuario)){
+                    usuario.setNombreUsuario(nombreUsuario);
+                }else{
+                    NombreExistenteIH mensaje = new NombreExistenteIH();
+                    mensaje.mostrarMensaje();
+                }
+            }
+            
+            if (contrasenia != null && !contrasenia.isEmpty()) {
+                usuario.setContrasenia(contrasenia);
+            }
+            
+            if (correo != null && !correo.isEmpty()) {
+                if(!udb.existeCorreo(correo)){
+                usuario.setCorreo(correo);
+                }else{
+                    CorreoExistenteIH mensaje = new CorreoExistenteIH();
+                    mensaje.mostrarMensaje();
+                }
+            }
+        
+            udb.actualizar(usuario);
+        }
     }
 
-    public void eliminarCuenta() {
+    public void borrarCuenta(){
+        UsuarioDAO udb = new UsuarioDAO();
+        Usuario cv = udb.consultarId(idUsuario);
+        udb.borrar(cv);
     }
 
 }
