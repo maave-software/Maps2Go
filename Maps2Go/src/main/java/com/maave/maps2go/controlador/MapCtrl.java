2fc6bf75f146915025b6d7afdcb9acd1092b54d5
ValeLanda <valeriaa.landaa@ciencias.unam.mx>
2019-04-07 00:30:00 -0600

    Marcador Modificado

--- Maps2Go/src/main/java/com/maave/maps2go/controlador/MapCtrl.java
+++ Maps2Go/src/main/java/com/maave/maps2go/controlador/MapCtrl.java
@@ -5,10 +5,15 @@
  */
 package com.maave.maps2go.controlador;
 
+import com.maave.maps2go.modelo.Marcador;
+import com.maave.maps2go.modelo.MarcadorDAO;
+import com.maave.maps2go.modelo.Tema;
+import com.maave.maps2go.modelo.TemaDAO;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import org.primefaces.event.map.MarkerDragEvent;
+import org.primefaces.event.map.OverlaySelectEvent;
 import org.primefaces.event.map.PointSelectEvent;
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
@@ -24,52 +29,75 @@ import org.primefaces.model.map.Marker;
 @ViewScoped
 public class MapCtrl {
     private Marker marcador;
+    private Marker marker;
     private MapModel simpleModel;
+    private String datosUtiles;
+    private String descripcion;
     private double latitud;
-    private double longitud;   
+    private double longitud;
+    private int numMarcador;
+    private Tema tema;
+
+    public String getDatosUtiles() {
+        return datosUtiles;
+    }
+
+    public void setDatosUtiles(String datosUtiles) {
+        this.datosUtiles = datosUtiles;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
     
-   
+    
     @PostConstruct
     public void init(){
         simpleModel = new DefaultMapModel();
-        marcador = new Marker(new LatLng(23.382390, -102.291477),"Arrastrame");
+        marcador = new Marker(new LatLng(23.382390, -102.291477));
         marcador.setDraggable(true);
-        //marcador.setClickable(true);
         simpleModel.addOverlay(marcador);
         this.latitud = marcador.getLatlng().getLat();
         this.longitud = marcador.getLatlng().getLng();
     }
-    
+
     public Marker getMarcador() {
         return marcador;
     }
-    
+
     public MapModel getSimpleModel() {
         return simpleModel;
     }
     
-    public double getLongitud(){
-        return this.longitud;
+    public void onMarkerSelect(OverlaySelectEvent event) {
+       marker =(Marker) event.getOverlay(); 
     }
-    
-    public void setLongitud(double longitud){
-        this.longitud = longitud;
-    }
-    
-    public double getLatitud(){
-        return this.latitud;
-    }
-    
-    public void setLatitud(double latitud){
-        this.latitud = latitud;
-    }
-    
     public void onMarkerDrag(MarkerDragEvent event){
         marcador = event.getMarker();
         this.latitud = marcador.getLatlng().getLat();
         this.longitud = marcador.getLatlng().getLng();
     }
-    
+
     public void onPointSelect(PointSelectEvent event) {
         LatLng latlng = event.getLatLng();
         marcador = simpleModel.getMarkers().get(0);
@@ -78,4 +106,19 @@ public class MapCtrl {
         this.longitud = latlng.getLng();
         
     }
+    
+     public void agregarMarcador() {
+        MarcadorDAO mdb = new MarcadorDAO();
+        Marcador m = new Marcador();
+        TemaDAO tdb = new TemaDAO();
+        Tema t = new Tema();
+        t = tdb.buscaTema(tema.getTipoTema());
+        m.setDescripcion(descripcion);
+        m.setDatosUtiles(datosUtiles);
+        m.setLatitud(latitud);
+        m.setLongitud(longitud);
+        m.setTema(t);
+        mdb.agregar(m);
+    }
+  
 }
